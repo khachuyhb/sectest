6ca01cfd2f1a95c1c7e7a8d984886a3a23b1a219
Huy Khắc <khachuyhb@gmail.com>
2023-10-19 14:02:58 +0700

    first commit

---
+++ Controllers/brandController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoesLover.Data;
+using ShoesLover.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shoeslover.Controllers
+{
+    public class brandController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+
+        public IActionResult FilterBrand()
+        {
+            StoreContext context = HttpContext.RequestServices.GetService(typeof(ShoesLover.Data.StoreContext)) as StoreContext;
+
+            return View(context.GetBrand());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Builder;
+
+namespace ShoesLover
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    
+                    /*webBuilder.UseKestrel(kestrelServerOptions =>
+                    {
+                        kestrelServerOptions.Listen(IPAddress.Any, 8090);
+                        kestrelServerOptions.ListenLocalhost(8091);
+                        kestrelServerOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(2);
+                        // Các thiết lập cho Kestrel tại đây
+                        // sử dụng KestrelServerOptons để thiết lập
+                    });
+                    webBuilder.UseUrls("http://0.0.0.0:8090", "https://0.0.0.0:8091");
+                    webBuilder.UseUrls("http://0.0.0.0:5000");*/
+                    webBuilder.UseStartup<Startup>();
+                   
+                });
+
+    }
+}
